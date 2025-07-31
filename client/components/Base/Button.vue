b7971ac7ad846ed234e0349eb926e5ce756e57ad
Martin Hanzl <martas.hanzl@email.cz>
2025-07-31 09:07:40 +0200

    client design colors & favicon update

--- client/components/Base/Button.vue
+++ client/components/Base/Button.vue
@@ -8,7 +8,7 @@ defineProps({
 </script>
 
 <template>
-<button type="button" :class="[color === 'primary' ? 'bg-indigo-600 text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-indigo-600' : 'bg-white hover:bg-indigo-500 focus-visible:outline-indigo-600', 'inline-flex justify-center cursor-pointer rounded-md px-3 py-2 text-sm font-semibold focus-visible:outline-2 focus-visible:outline-offset-2 sm:col-start-2 border border-indigo-500']">
+<button type="button" :class="[color === 'primary' ? 'bg-green-600 text-white shadow-xs hover:bg-green-500 focus-visible:outline-green-600' : 'bg-white hover:bg-green-500 focus-visible:outline-green-600', 'inline-flex justify-center cursor-pointer rounded-md px-3 py-2 text-sm font-semibold focus-visible:outline-2 focus-visible:outline-offset-2 sm:col-start-2 border border-green-500']">
   <slot />
 </button>
 </template>
--- client/components/Form/Input.vue
+++ client/components/Form/Input.vue
@@ -24,7 +24,7 @@ defineProps({
   <div>
     <label for="email" class="block text-sm/6 text-left font-medium text-gray-900">{{ label }}</label>
     <div class="mt-2">
-      <Field v-model="model" :type="type" :name="name" :id="name" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+      <Field v-model="model" :type="type" :name="name" :id="name" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-green-600 sm:text-sm/6" />
     </div>
   </div>
 </template>
